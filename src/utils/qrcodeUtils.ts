ae545fa19b04e94999f8fe8bda510bea29b89d37
Burhan-sheikh <epithet02@gmail.com>
2025-09-26 12:03:30 +0530

    Updated index.css

--- src/utils/qrcodeUtils.ts
+++ src/utils/qrcodeUtils.ts
@@ -1,4 +1,5 @@
 import { QRCodeData } from '../types/qrcode';
+import { EnhancedQRCodeData, generateQRValue as generateEnhancedQRValue } from '../types/qrContentTypes';
 
 export const generateQRValue = (qrData: QRCodeData): string => {
     if (qrData.type === 'static') {
@@ -8,4 +9,12 @@ export const generateQRValue = (qrData: QRCodeData): string => {
         return `${window.location.origin}/r/${dynamicQR.shortId}`;
     }
     return '';
+};
+
+export const generateEnhancedQRValue = (qrData: EnhancedQRCodeData): string => {
+    if (qrData.qrType === 'static') {
+        return generateEnhancedQRValue(qrData.content);
+    } else {
+        return `${window.location.origin}/e/${qrData.shortId}`;
+    }
 };
