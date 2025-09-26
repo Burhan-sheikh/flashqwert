cc694349449deeb19b78c9562a42c8ab139ea398
Burhan-sheikh <epithet02@gmail.com>
2025-09-26 12:22:35 +0530

    Updated QRGenerator.tsx

--- src/pages/QRGenerator.tsx
+++ src/pages/QRGenerator.tsx
@@ -61,7 +61,7 @@ const QRGeneratorPage: React.FC = () => {
   if (loading || isLoading) {
     return (
       <div className="min-h-screen bg-gray-50">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="px-4 sm:px-6 lg:px-8">
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
